3f2aa57e08a30ce4bfe6ad674da6669a19b78be1
nash <fj0rd@qq.com>
2025-04-16 22:21:56 +0800

    horizontal Container

--- src/libs/components/container.rs
+++ src/libs/components/container.rs
@@ -1,21 +1,34 @@
+use super::super::data::Layout;
 use dioxus::prelude::*;
 use dioxus_logger::tracing;
-use super::super::data::Layout;
+use serde_json::{from_str, to_value};
 
 #[component]
-pub fn Container(layout:Layout, children: Element) -> Element {
-    rsx!{
+pub fn Container(layout: Layout, children: Element) -> Element {
+    let a = layout.attrs.unwrap_or(from_str("{}").unwrap());
+    let a = a.as_object().unwrap();
+    let mut css = vec!["Container", "f"];
+
+    let h = a
+        .get("horizontal")
+        .and_then(|x| x.as_bool())
+        .unwrap_or(false);
+    if !h {
+        css.push("v");
+    }
+    let css = css.join(" ");
+
+    rsx! {
         div {
-            class: "Container f v",
+            class: css,
             {children}
         }
     }
 }
 
-
 #[component]
 pub fn List(layout: Layout, children: Element) -> Element {
-    rsx!{
+    rsx! {
         div {
             class: "List f v",
             {children}
