79c6fa5e894dd53d08f1483bd66b5fc1d22e83f9
nash <fj0rd@qq.com>
2025-04-16 22:28:00 +0800

    Container class

--- src/libs/components/container.rs
+++ src/libs/components/container.rs
@@ -5,22 +5,34 @@ use serde_json::{from_str, to_value};
 
 #[component]
 pub fn Container(layout: Layout, children: Element) -> Element {
-    let a = layout.attrs.unwrap_or(from_str("{}").unwrap());
-    let a = a.as_object().unwrap();
     let mut css = vec!["Container", "f"];
 
-    let h = a
-        .get("horizontal")
-        .and_then(|x| x.as_bool())
-        .unwrap_or(false);
-    if !h {
-        css.push("v");
-    }
-    let css = css.join(" ");
+    let a = layout.attrs.unwrap_or(from_str("{}").unwrap());
+    if let Some(a) = a.as_object() {
+        let h = a
+            .get("horizontal")
+            .and_then(|x| x.as_bool())
+            .unwrap_or(false);
+        if !h {
+            css.push("v");
+        }
+        let cc = a.get("class").and_then(|x| x.as_str()).unwrap_or("");
+        css.push(cc);
+    };
 
     rsx! {
         div {
-            class: css,
+            class: css.join(" "),
+            {children}
+        }
+    }
+}
+
+#[component]
+pub fn Card(layout: Layout, children: Element) -> Element {
+    rsx! {
+        div {
+            class: "Card f v box border shadow",
             {children}
         }
     }
@@ -36,12 +48,3 @@ pub fn List(layout: Layout, children: Element) -> Element {
     }
 }
 
-#[component]
-pub fn Card(layout: Layout, children: Element) -> Element {
-    rsx! {
-        div {
-            class: "Card f v box border shadow",
-            {children}
-        }
-    }
-}
