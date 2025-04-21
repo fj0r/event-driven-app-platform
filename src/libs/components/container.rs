e9c4e40bc67c28892f15ce1ec85b5493f963896a
nash <fj0rd@qq.com>
2025-04-21 17:37:20 +0800

    feat: render list

--- src/libs/components/container.rs
+++ src/libs/components/container.rs
@@ -32,32 +32,44 @@ pub fn Container(layout: Layout, children: Element) -> Element {
 
 #[component]
 pub fn List(layout: Layout, children: Element) -> Element {
-    let c = if let Some(i) = layout.item {
-        let x = i[0].clone();
+    let mut css = vec!["List", "f"];
+    let l = layout.clone();
+    let a = unwrap_or_object(l.attrs);
+    if let Some(a) = a.as_object() {
+        let h = a
+            .get("horizontal")
+            .and_then(|x| x.as_bool())
+            .unwrap_or(false);
+        if !h {
+            css.push("v");
+        }
+        let cc = a.get("class").and_then(|x| x.as_str()).unwrap_or("");
+        css.push(cc);
+    };
 
-        let s = use_context::<Store>();
-        let v = use_memo(move || {
-            let t = if let Some(b) = &layout.data {
-                if !b.upload {
-                    let x = s.list.read().get(&b.event).cloned();
-
-                }
-
-            };
-        });
+    let s = use_context::<Store>();
+    let i = &layout.clone().item.context("item")?[0];
+    let b = layout.data.clone().context("data")?;
+    let c = s.list.read();
+    let c = c.get(&b.event).cloned().unwrap_or_else(|| Vec::new());
+    let r = c.iter().enumerate().map(|(idx, child)| {
+        let x = rsx! {
+            Dynamic {
+                layout: child.clone()
+            }
+        };
         rsx! {
             Dynamic {
-                layout: x,
-                {children}
+                key: idx,
+                layout: i.clone(),
+                {x}
             }
         }
-    } else {
-        children
-    };
+    });
     rsx! {
         div {
-            class: "List f v",
-            {c}
+            class: css.join(" "),
+            {r}
         }
     }
 }
