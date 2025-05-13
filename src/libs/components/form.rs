d500ea78f0d984f57caea66c1c7ed15f8a2fdcf3
nash <fj0rd@qq.com>
2025-05-13 17:19:02 +0800

    walk

--- src/libs/components/form.rs
+++ src/libs/components/form.rs
@@ -1,19 +1,24 @@
-use super::super::data::Layout;
+use super::super::data::{Layout, Bind};
 use super::super::store::Store;
-use super::utils::merge_css_class;
 use super::{Dynamic, Frame};
 use dioxus::prelude::*;
+
+fn walk(layout: &Layout) {
+
+}
 
 #[component]
 pub fn Form(layout: Layout) -> Element {
     dioxus_logger::tracing::info!("{layout:?}");
 
     let children = layout.clone().children.unwrap_or_else(|| vec![]);
+    walk(&layout);
     let children = children.into_iter().map(|c| {
         rsx! {
             Frame { layout: c }
         }
     });
+    layout.kind = "container".to_owned();
     rsx! {
         Dynamic {
             layout: layout,
