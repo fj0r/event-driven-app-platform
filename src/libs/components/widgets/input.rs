553967ed00bc3d897425865486705360e7409bb0
nash <fj0rd@qq.com>
2025-05-17 10:03:04 +0800

    Update CSS structure and remove redundant classes
    - Reorganized CSS sections for better readability and structure
    - Removed redundant `.txt` class and moved its styles to a new section
    - Added new utility classes like `.overlay`, `.abs`, and `.pointer`
    - Improved code formatting and comments in the CSS file

--- src/libs/components/widgets/input.rs
+++ src/libs/components/widgets/input.rs
@@ -58,7 +58,7 @@ pub fn Input(layout: Layout) -> Element {
     };
 
     let s = use_context::<Store>();
-    let mut css = vec!["input", "f", "shadow"];
+    let mut css = vec!["input"];
     let css = merge_css_class(&mut css, &layout);
 
     let oninput = move |event: Event<FormData>| {
