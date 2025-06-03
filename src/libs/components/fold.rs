067feaf4f38036921afa586e836746bf43c04d6b
nash <fj0rd@qq.com>
2025-06-03 18:51:42 +0800

    Refactor fold component to improve styling and structure - Changed CSS class from 'f v' to 'g' - Added grid layout for better alignment - Improved icon and content structure - Simplified show/hide logic

--- src/libs/components/fold.rs
+++ src/libs/components/fold.rs
@@ -5,7 +5,7 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn Fold(layout: Layout, children: Element) -> Element {
-    let mut css = vec!["f", "v"];
+    let mut css = vec!["g"];
     let css = merge_css_class(&mut css, &layout);
 
     let Some((replace_header, float_body)) = layout.attrs.as_ref().map(|x| {
@@ -32,48 +32,50 @@ pub fn Fold(layout: Layout, children: Element) -> Element {
             .unwrap_or_default()
     });
 
-    let b = if show() {
-        if replace_header {
-            rsx! {
-                div {
-                    class: "f v",
-                    {children}
-                }
-            }
-        } else {
-            rsx! {
+    let onclick = move |_event| {
+        let mut s = show.clone();
+        s.set(!show());
+    };
+
+    let h = if replace_header && show() {
+        rsx! { div {} }
+    } else {
+        rsx! {
+            div {
                 Frame { layout: item }
-                div {
-                    class: "f v",
-                    {children}
-                }
+            }
+        }
+    };
+    let b = if show() {
+        rsx! {
+            div {
+                {children}
             }
         }
     } else {
-        rsx! {
-            Frame { layout: item }
-        }
+        rsx! { div {} }
     };
 
     let icon_style = r#"
-        width: 1em;
+        height: 100%;
+        aspect-ratio: 1 / 1;
     "#;
+    let grid_style = r#"
+        grid-template-columns: auto 1fr;
+        "#;
     let icon_class = if show() { "icon open" } else { "icon close " };
     rsx! {
         div {
-            class: "f",
+            class: css.join(" "),
+            style: "{grid_style}",
+            onclick: onclick,
             div {
                 class: icon_class,
                 style: "{icon_style}",
             },
-            div {
-                onclick: move |_event| {
-                    let mut s = show.clone();
-                    s.set(!show());
-                },
-                class: css.join(" "),
-                {b}
-            }
+            {h},
+            div {},
+            {b}
         }
     }
 }
