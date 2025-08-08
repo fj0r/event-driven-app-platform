6425be398103e23e4d300bb87ba162106f5d24b0
fj0r <fj0rd@qq.com>
2025-08-08 16:33:47 +0800

    Implement chat message processing and update documentation
    - Add chat setup commands to README
    - Replace debug output with message processing logic
    - Add envelope sending for valid chat messages

--- README.md
+++ README.md
@@ -3,4 +3,14 @@
 ```nu
 use __.nu
 __ serve --rpk
+
 ```
+
+
+## chat
+```nu
+__ pg up
+__ pg migrate
+__ chat up
+```
+
--- chat/src/libs/logic/chat.rs
+++ chat/src/libs/logic/chat.rs
@@ -1,6 +1,21 @@
 use super::super::handler::{ArcShared, ChatMessage, Envelope, Sender};
+use message::session::Session;
 use std::fmt::Debug;
 
 pub async fn chat<T: Debug>(e: ChatMessage<T>, s: ArcShared, x: Sender<T>) {
-    println!("chat => {:?}", e);
+    let ChatMessage {
+        sender,
+        created,
+        content,
+    } = &e;
+
+    if let Some(content) = content.as_object()
+        && let Some(d) = content.get("data")
+        && let Some(d) = d.as_str()
+    {
+        let _ = x.send(Envelope {
+            receiver: vec![sender.clone()],
+            message: e,
+        });
+    };
 }
