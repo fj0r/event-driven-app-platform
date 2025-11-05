95e6b23fba425d8eb76f879f3f0e791911700cfd
fj0r <fj0rd@qq.com>
2025-11-05 16:52:32 +0800

    Update example to use JsonComponent schema
    - Changed schema generation from `Component` to `JsonComponent`
    - Maintains same functionality with updated type reference

--- component/examples/componentschema.rs
+++ component/examples/componentschema.rs
@@ -2,7 +2,7 @@ use component::JsonComponent;
 use schemars::schema_for;
 
 fn main() -> Result<(), &'static dyn std::error::Error> {
-    let schema = schema_for!(Component);
+    let schema = schema_for!(JsonComponent);
     println!("{}", serde_json::to_string_pretty(&schema).unwrap());
     Ok(())
 }
