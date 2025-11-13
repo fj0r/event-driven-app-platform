bee56933797c9eecd39e834ef494d3edac405b5c
fj0r <fj0rd@qq.com>
2025-11-13 16:15:20 +0800

    Enhance macro generation with enum dispatch support
    - Added HashMap import for component info mapping
    - Extended gen_ function to handle enum-based component dispatch
    - Implemented dynamic RSX generation with ID management
    - Updated test to reflect new dispatch logic and output format

--- ui_macro/src/lib.rs
+++ ui_macro/src/lib.rs
@@ -1,11 +1,13 @@
+use std::collections::HashMap;
+
 use anyhow::anyhow;
 use proc_macro::TokenStream;
-use proc_macro2::TokenStream as TokenStream2;
+use proc_macro2::{Ident, Span, TokenStream as TokenStream2};
 use quote::quote;
 use syn::parse_file;
 mod configlist;
 mod walk;
-use walk::walk;
+use walk::{CompInfo, walk};
 mod utils;
 
 #[proc_macro]
@@ -13,9 +15,61 @@ pub fn gen_dispatch(input: TokenStream) -> TokenStream {
     quote! {}.into()
 }
 
-fn gen_(ast: &syn::File) -> syn::Result<TokenStream2> {
+fn gen_(ast: &syn::File, entry: &str) -> syn::Result<TokenStream2> {
     let info = walk(&ast);
-    Ok(quote! {#ast})
+    let ty = Ident::new(entry, Span::call_site());
+    let CompInfo::Enum { fields } = info
+        .get(entry)
+        .ok_or(syn::Error::new(Span::call_site(), "no fields"))?
+    else {
+        return Err(syn::Error::new(Span::call_site(), "no enum"));
+    };
+    let f = fields.iter().map(|x| {
+        let var = Ident::new(&x.name, Span::call_site());
+        let var_ = Ident::new(&format!("{}_", &x.name), Span::call_site());
+        let has_child = info
+            .get(&x.r#type)
+            .ok_or(syn::Error::new(Span::call_site(), "get child failed"));
+        let Ok(CompInfo::Struct { name: _, has_child }) = has_child else {
+            // return Err(syn::Error::new(Span::call_site(), "no child"));
+            panic!("no child")
+        };
+        let children = if *has_child {
+            quote! {
+                {children}
+            }
+        } else {
+            quote! {}
+        };
+        let id = if x.has_id {
+            let id = Ident::new(&format!("{}_id", &x.name).to_uppercase(), Span::call_site());
+            quote! {
+                static #id: LazyLock<Mutex<u32>> = LazyLock::new(|| Mutex::new(0));
+                let mut tc = #id.lock().unwrap();
+                *tc += 1;
+                let id = format!("#id-{}", *tc);
+            }
+        } else {
+            quote! {}
+        };
+
+        quote! {
+            #ty::#var(c) => {
+                #id
+                rsx!(#var_ {
+                    id: id,
+                    component: c,
+                    #children
+                })
+            }
+        }
+    });
+
+    Ok(quote! {
+        match component {
+            #(#f),*
+        }
+    })
 }
 
 #[cfg(test)]
@@ -27,7 +81,9 @@ mod tests {
         let d = include_str!("../../component/src/lib.rs");
         let ast = parse_file(&d).unwrap();
         let _ = std::fs::write("../data/component_def.ast", format!("{:#?}", ast));
-        let output = walk(&ast);
-        let _ = std::fs::write("../data/dispatch_def.rs", format!("{:#?}", output));
+        let info = walk(&ast);
+        let _ = std::fs::write("../data/info.rs", format!("{:#?}", info));
+        let output = gen_(&ast, "JsonComponent").unwrap();
+        let _ = std::fs::write("../data/dispatch_def.rs", format!("{}", output.to_string()));
     }
 }
